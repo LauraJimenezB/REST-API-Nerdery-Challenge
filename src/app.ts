b32349f6e307ba0f4963c67f7113fc4f22cb9d47
DianaSanchezordonez <dianaordonez1998@gmail.com>
2021-06-02 13:18:39 -0500

    changed eslint configuration

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express'
 
 const app = express()
+const port: string | number = process.env.PORT || 3000
 
 app.get('/', (req: any, res: any) => {
   res.send('hello')
 })
-const port: string | number = process.env.PORT || 3000
-app.listen(port, () => console.log(`App listening on PORT ${port}`))
 
-console.log('aidaa123...')
+app.listen(port, () => console.log(`App listening on PORT ${port}`))
