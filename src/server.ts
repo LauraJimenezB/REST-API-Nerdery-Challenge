84d8d2ca3c5fc06a25580c1e3362999045b1d60b
DianaSanchezordonez <dianaordonez1998@gmail.com>
2021-06-03 15:30:28 -0500

    validation /post

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,43 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import express from 'express'
+import { Prisma, PrismaClient } from '@prisma/client';
+import express from 'express';
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/signup', async(req, res) => {
-  const { username, email } = req.body
+app.post('/signup', async (req, res) => {
+  const { username, email } = req.body;
 
   const result = await prisma.user.create({
     data: {
       username,
-      email
-    }
-  })
-  res.send(result)
-})
+      email,
+    },
+  });
+  res.send(result);
+});
 
-app.post('/posts', async(req, res) => {
-  const { title, content, authorId } = req.body
+app.post('/posts', async (req, res) => {
+  const { title, content, authorId } = req.body;
 
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      authorId
-    }
-  })
-  res.send(result)
-})
+  try {
+    const result = await prisma.post.create({
+      data: {
+        author: { connect: { id: authorId } },
+        title,
+        content,
+      },
+    });
+    res.send(result);
+  } catch (error) {
+    res.json({ error: `Not found userID ${authorId}` });
+  }
+});
 
-app.get('/users', async(req, res) => {
-  const result = await prisma.user.findMany()
-  res.send(result)
-})
+app.get('/users', async (req, res) => {
+  const result = await prisma.user.findMany();
+  res.send(result);
+});
 
-export { app }
-
-
+export { app };
