c679884cd962129177beb5dafc6b7d04aac1bfc1
DianaSanchezordonez <dianaordonez1998@gmail.com>
2021-06-04 11:07:10 -0500

    setup fail of jwt

--- prisma/migrations/20210604135637_update_data/migration.sql
+++ prisma/migrations/20210604135637_update_data/migration.sql
@@ -2,7 +2,7 @@
   Warnings:
 
   - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `fullname` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -13,8 +13,9 @@ ALTER TABLE "Post" ALTER COLUMN "updatedAt" DROP NOT NULL;
 
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "bio" VARCHAR(200),
-ADD COLUMN     "fullname" VARCHAR(150) NOT NULL,
-ADD COLUMN     "fullnameIsPublic" BOOLEAN NOT NULL DEFAULT false;
+ADD COLUMN     "fullname" VARCHAR(150),
+ADD COLUMN     "fullnameIsPublic" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "password" TEXT NOT NULL;
 
 -- DropTable
 DROP TABLE "Profile";
--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,11 @@
 import { PrismaClient } from '@prisma/client';
+import express, { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import { app } from './server';
 
 const prisma = new PrismaClient();
-const token = jwt.sign({ foo: 'bar' }, 'shhhh');
+
+app.use(express.json());
 
 interface User {
   id: number;
@@ -10,18 +13,53 @@ interface User {
   email: string;
 }
 
-export const newToken = (user: User): string => {
-  return jwt.sign({ id: user.id }, 'secret', {
-    expiresIn: '100d',
-  });
-};
+app.post('/signin', (req, res) => {
+  const { username, email } = req.body;
+  const user = { username, email };
+ 
+});
 
-export const verifyToken = async (token: string) => {
-  await jwt.verify(token, 'secret', (err, payload) => {
-    if (err) return err;
-    return payload;
+// jwt.sign({ user: user }, 'secret', { expiresIn: '1d' }, (err: any, token: any) => {
+//   res.json({
+//     token,
+//   });
+// });
+
+// Authorization: Bearer <token>
+// const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+//   const bearerHeader = req.headers['authorization'];
+
+//   if (typeof bearerHeader !== undefined) {
+//     const bearerToken = bearerHeader?.split('')[1];
+//     req.token = bearerToken;
+//     next();
+//   } else {
+//     res.send({ status: 403 });
+//   }
+// };
+
+// export const newToken = (user: User): string => {
+//   return jwt.sign({ id: user.id }, 'secret', {
+//     expiresIn: '1d',
+//   });
+// };
+
+// export const verifyToken = async (token: string) => {
+//   await jwt.verify(token, 'secret', (err, payload) => {
+//     if (err) return err;
+//     return payload;
+//   });
+// };
+
+app.post('/signin', (req, res) => {
+  const { username, email } = req.body
+  const user = { username, email }
+  jwt.sign({ user: user}, 'secret', (err: any, token: any) => {
+    res.json({
+      token
+    })
   });
-};
+})
 
 // console.log(
 //   newToken({
@@ -30,6 +68,3 @@ export const verifyToken = async (token: string) => {
 //     email: 'prueba@test.com',
 //   }),
 // );
-
-verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjIyNzYzODA1LCJleHAiOjE2MzE0MDM4MDV9.DYR1je203pvPd39uWtWFWvzfMqc2j5oYHDax5442G38')
-.then( data => console.log(data))
